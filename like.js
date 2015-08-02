65d09f5210c08d63992f878b683429a9a1f839f3
Gabriel Cohen <glcohen@ucsd.edu>
2015-08-02 00:22:03 -0400

    fixed icons

--- like.js
+++ like.js
@@ -1,7 +1,7 @@
-// No copyright, but don't copy, right?
+// My first time using js, be kind
+var clicks = 0; // click counter
 
-var clicks = 0;
-
+// Make sure this only runs on facebook
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.indexOf("facebook.com") > -1) {
         chrome.pageAction.show(tabId);
@@ -11,20 +11,21 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 // Called when the user clicks on the page action.
 chrome.pageAction.onClicked.addListener(function(tab) {
   if (clicks == 0) {
-    chrome.pageAction.setIcon({path: "dontlike.png", tabId: tab.id});
-    chrome.pageAction.setTitle({title: "idontlike", tabId: tab.id});
-    chrome.tabs.executeScript({
+    chrome.pageAction.setIcon({path: "dontlike.png", tabId: tab.id}); // Update icon
+    chrome.pageAction.setTitle({title: "idontlike", tabId: tab.id}); // Update title
+    chrome.tabs.executeScript({ // Hide like buttons
       code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display="none"; }'
     });
-  } else {
-    chrome.pageAction.setIcon({path: "like.png", tabId: tab.id});
-    chrome.pageAction.setTitle({title: "like", tabId: tab.id});
-    chrome.tabs.executeScript({
+  } 
+  else {
+    chrome.pageAction.setIcon({path: "like.png", tabId: tab.id}); // Update icon
+    chrome.pageAction.setTitle({title: "like", tabId: tab.id}); // Update title
+    chrome.tabs.executeScript({ // Show like buttons
       code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display=""; }'
     });
   }
   
-  // wrap around
+  // wrap coutner around
   clicks++;
   if (clicks > 1)
     clicks = 0;
