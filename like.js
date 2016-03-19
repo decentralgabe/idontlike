c8f8098974641fd8059dddf56c6e26505d1d25a5
Gabriel Cohen <glcohen@ucsd.edu>
2016-03-19 15:36:41 -0700

    a little buggy, but it has persistence now

--- like.js
+++ like.js
@@ -1,10 +1,25 @@
-// My first time using js, be kind
 var clicks = 0; // click counter
 
 // Make sure this only runs on facebook
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.indexOf("facebook.com") > -1) {
         chrome.pageAction.show(tabId);
+    }
+
+    if (clicks == 0) {
+      chrome.pageAction.setIcon({path: "dontlike.png", tabId: tab.id}); // Update icon
+      chrome.pageAction.setTitle({title: "idontlike", tabId: tab.id}); // Update title
+      chrome.tabs.executeScript({ // Hide like buttons
+        code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display="none"; }'
+      });
+    }
+
+    else {
+      chrome.pageAction.setIcon({path: "like.png", tabId: tab.id}); // Update icon
+      chrome.pageAction.setTitle({title: "like", tabId: tab.id}); // Update title
+      chrome.tabs.executeScript({ // Show like buttons
+        code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display=""; }'
+      });
     }
 });
 
@@ -16,7 +31,8 @@ chrome.pageAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript({ // Hide like buttons
       code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display="none"; }'
     });
-  } 
+  }
+
   else {
     chrome.pageAction.setIcon({path: "like.png", tabId: tab.id}); // Update icon
     chrome.pageAction.setTitle({title: "like", tabId: tab.id}); // Update title
@@ -24,10 +40,9 @@ chrome.pageAction.onClicked.addListener(function(tab) {
       code: 'var like = document.getElementsByClassName("UFILikeLink"); for (index = 0; index < like.length; ++index) { like[index].style.display=""; }'
     });
   }
-  
+
   // wrap coutner around
   clicks++;
   if (clicks > 1)
     clicks = 0;
 });
-
